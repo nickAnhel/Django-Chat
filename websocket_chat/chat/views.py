997eeeee8d52e0196b7415b06bef32c25f178841
nickAnhel <anhimovn1@gmail.com>
2024-04-20 01:28:42 +0300

    Added auth protection to views

--- websocket_chat/chat/views.py
+++ websocket_chat/chat/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
 
 from .models import Chat
 from .forms import ChatCreateForm
@@ -12,17 +13,20 @@ def chat_list(request: HttpRequest) -> HttpResponse:
     return render(request, "chat/list.html", {"chats": chats})
 
 
+@login_required
 def chat_detail(requst: HttpRequest, chat_id: int) -> HttpResponse | HttpResponseRedirect:
     chat: Chat = get_object_or_404(Chat, pk=chat_id)
     return render(requst, "chat/detail.html", {"chat": chat})
 
 
+@login_required
 def chat_join(request: HttpRequest, chat_id: int) -> HttpResponseRedirect:
     chat: Chat = get_object_or_404(Chat, pk=chat_id)
     chat.members.add(request.user)
     return HttpResponseRedirect(reverse("chat:detail", kwargs={"chat_id": chat_id}))
 
 
+@login_required
 def chat_create(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     if request.method == "POST":
         chat_create_form = ChatCreateForm(data=request.POST)
